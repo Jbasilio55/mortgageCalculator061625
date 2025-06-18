e2217cc47540a02a8e51dfbbbc775c41bbd75a18
Jbasilio55 <j.l.basilio55@gmail.com>
2025-06-18 19:05:02 -0400

    Completed mortgage calculator after adding while loops for values to stay within a reasonable amount

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -11,20 +11,55 @@ public class Main {
 
         //open scanner
         Scanner scanner = new Scanner(System.in);
+        int principal; // initialize principal
 
         // get total amount of principal
-        System.out.print("Principal: ");
-        int principal  = scanner.nextInt();
+        while(true){
+            System.out.print("Principal ($1k - $1M): ");
+            principal  = scanner.nextInt();
+
+            // if principal is between 1k - 1M (break)
+            if(principal >= 1000 && principal <= 1000000){
+                break;
+            }else{
+                System.out.println("Enter number between 1,000 and 1,000,000");
+            }
+        }
+
+        double annualRate; // initialize annual rate
+        double monthlyRate; // initialize monthly rate
 
         //get annual rate
-        System.out.print("Annual Interest Rate: ");
-        double annualRate = scanner.nextDouble() / percent;
-        double monthlyRate = annualRate / monthsInAYear;
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            annualRate = scanner.nextDouble();
+
+            // if annual rate is between 1 - 30 (break)
+            if(annualRate >= 1 && annualRate <= 30){
+                annualRate /= percent;
+                monthlyRate = annualRate / monthsInAYear;
+                break;
+            }else{
+                System.out.println("Enter a value greater that 0 and less than or equal to 30");
+            }
+        }
+
+        int period; // initialize period
+        int numberOfPayments; // initialize number of payments
 
         //get year and then convert to number of payment by multiplying -> (years * months)
-        System.out.print("Period (Years): ");
-        int period = scanner.nextInt();
-        int numberOfPayments  = period * monthsInAYear;
+        while(true){
+            System.out.print("Period (Years): ");
+            period = scanner.nextInt();
+
+            // if period is between 1 - 30 (break)
+            if(period >= 1 && period <= 30){
+                numberOfPayments  = period * monthsInAYear;
+                break;
+            }else{
+                System.out.println("Enter a value between 1 and 30");
+            }
+        }
 
         //mortgage formula
         double mortgage = principal *
