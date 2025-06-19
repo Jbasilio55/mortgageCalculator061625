0cb557de7c623c74671f675a330d155a208dbbb3
Jbasilio55 <j.l.basilio55@gmail.com>
2025-06-18 22:58:50 -0400

    cleaning up code and made two helper methods for input prompt and mortgage calculation.

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,75 +3,69 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //title
+        Scanner scanner = new Scanner(System.in);
         System.out.println("-----Mortgage Calculator----\n");
 
-        byte percent = 100; // 100%
-        byte monthsInAYear = 12; // 12 month -> 1 year
+        int principal;
+        double annualRate;
+        int period;
 
-        //open scanner
-        Scanner scanner = new Scanner(System.in);
-        int principal; // initialize principal
+        //Get principal
+        principal = (int)readNumber(
+                "Principal ($1k - $1M): ",
+                1000,
+                1_000_000,
+                "Enter number between 1,000 and 1,000,000"
+        );
 
-        // get total amount of principal
-        while(true){
-            System.out.print("Principal ($1k - $1M): ");
-            principal  = scanner.nextInt();
+        // Get annual interest rate
+        annualRate = readNumber(
+                "Annual Interest Rate: ",
+                1,
+                30,
+                "Enter a value greater than 0 and less than or equal to 30"
+        );
 
-            // if principal is between 1k - 1M (break)
-            if(principal >= 1000 && principal <= 1000000){
-                break;
-            }else{
-                System.out.println("Enter number between 1,000 and 1,000,000");
-            }
-        }
+        // Get loan period
+        period = (int)readNumber(
+                "Period (Years): ",
+                1,
+                30,
+                "Enter a value between 1 and 30"
+        );
 
-        double annualRate; // initialize annual rate
-        double monthlyRate; // initialize monthly rate
-
-        //get annual rate
-        while(true){
-            System.out.print("Annual Interest Rate: ");
-            annualRate = scanner.nextDouble();
-
-            // if annual rate is between 1 - 30 (break)
-            if(annualRate >= 1 && annualRate <= 30){
-                annualRate /= percent;
-                monthlyRate = annualRate / monthsInAYear;
-                break;
-            }else{
-                System.out.println("Enter a value greater that 0 and less than or equal to 30");
-            }
-        }
-
-        int period; // initialize period
-        int numberOfPayments; // initialize number of payments
-
-        //get year and then convert to number of payment by multiplying -> (years * months)
-        while(true){
-            System.out.print("Period (Years): ");
-            period = scanner.nextInt();
-
-            // if period is between 1 - 30 (break)
-            if(period >= 1 && period <= 30){
-                numberOfPayments  = period * monthsInAYear;
-                break;
-            }else{
-                System.out.println("Enter a value between 1 and 30");
-            }
-        }
-
-        //mortgage formula
-        double mortgage = principal *
-                (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments) /
-                        (Math.pow(1 + monthlyRate, numberOfPayments) - 1));
-
-        // format amount from double to dollar amount
+        // Call mortgage calculator
+        double mortgage = getMortgage(principal, annualRate, period);
         String formatMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-
         System.out.println("Mortgage: " + formatMortgage);
 
-        // close scanner
         scanner.close();
+    }
+
+    public static double readNumber(String prompt, int min, int max, String errorPrompt){
+        Scanner scanner = new Scanner(System.in);
+        double value;
+
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max)
+                break;
+            System.out.println(errorPrompt);
+        }
+
+        return value;
+    }
+
+    public static double getMortgage(int principal, double annualRate, int period) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        double monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR;
+        int numberOfPayments = period * MONTHS_IN_YEAR;
+
+        return principal *
+                (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)) /
+                (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
     }
 }
