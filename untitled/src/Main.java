4c0043d2fb545c99408e4af3e88495a64752a8c6
Jbasilio55 <j.l.basilio55@gmail.com>
2025-06-18 23:15:39 -0400

    cleaning up code to make it more readable and simple. still functions as normal

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,47 +3,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("-----Mortgage Calculator----\n");
 
-        int principal;
-        double annualRate;
-        int period;
-
         //Get principal
-        principal = (int)readNumber(
-                "Principal ($1k - $1M): ",
-                1000,
-                1_000_000,
-                "Enter number between 1,000 and 1,000,000"
-        );
+        int principal = (int)readNumber(
+                "Principal ($1k - $1M): ", 1000, 1_000_000);
 
         // Get annual interest rate
-        annualRate = readNumber(
-                "Annual Interest Rate: ",
-                1,
-                30,
-                "Enter a value greater than 0 and less than or equal to 30"
-        );
+        double annualRate = readNumber(
+                "Annual Interest Rate: ", 1, 30);
 
         // Get loan period
-        period = (int)readNumber(
-                "Period (Years): ",
-                1,
-                30,
-                "Enter a value between 1 and 30"
-        );
+        int period = (int)readNumber("Period (Years): ", 1, 30);
 
         // Call mortgage calculator
         double mortgage = getMortgage(principal, annualRate, period);
         String formatMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + formatMortgage);
-
-        scanner.close();
     }
 
-    public static double readNumber(String prompt, int min, int max, String errorPrompt){
+    public static double readNumber(String prompt, int min, int max){
         Scanner scanner = new Scanner(System.in);
+
         double value;
 
         while (true) {
@@ -51,7 +32,7 @@ public class Main {
             value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
-            System.out.println(errorPrompt);
+            System.out.println("Enter a value between "+ min +" and "+max);
         }
 
         return value;
