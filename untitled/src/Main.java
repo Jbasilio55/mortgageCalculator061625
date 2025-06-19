7249aa3ab6ecaf93045a65c1de5b744c61d7cf64
Jbasilio55 <j.l.basilio55@gmail.com>
2025-06-19 11:00:03 -0400

    added method to return each payment

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -20,6 +20,8 @@ public class Main {
         double mortgage = getMortgage(principal, annualRate, period);
         String formatMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + formatMortgage);
+
+        getLeftOverLoanAmount(principal, annualRate, period);
     }
 
     public static double readNumber(String prompt, int min, int max){
@@ -39,14 +41,34 @@ public class Main {
     }
 
     public static double getMortgage(int principal, double annualRate, int period) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12; // months in a year
+        final byte PERCENT = 100; // 100%
 
-        double monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR;
-        int numberOfPayments = period * MONTHS_IN_YEAR;
+        double monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR; //monthly rate
+        int numberOfPayments = period * MONTHS_IN_YEAR; // number of total payments
 
         return principal *
                 (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)) /
                 (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
+    }
+
+    private static void getLeftOverLoanAmount(int principal, double annualRate, int period) {
+        final byte MONTHS_IN_YEAR = 12; // months in a year
+        final byte PERCENT = 100; // 100%
+
+        double monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR; //monthly rate
+        int numberOfPayments = period * MONTHS_IN_YEAR; // number of total payments
+
+        System.out.println("\nPAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        // L[(1 + c)**n – (1 + c)**p]/[(1 + c)**n – 1]
+
+        for(int i = 1; i < numberOfPayments + 1; i++){
+            double amountOwed = principal *
+                    (Math.pow(1 + monthlyRate, numberOfPayments) - Math.pow(1 + monthlyRate, i)) /
+                    (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
+            System.out.println(NumberFormat.getCurrencyInstance().format(amountOwed));
+        }
     }
 }
